a60b7111404af455a752c7eabc4c6aa40b7ba535
Marcel Beck <marcel@beck.im>
2017-10-10 21:47:00 +0200

    Add pprof

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # HTTP Log Server for debugging purposes
 
 > It enables you to see the content of any http request you send against this service
+
+## pprof
+```sh
+echo "GET http://localhost:8080/" | vegeta attack -duration=60s | tee results.bin | vegeta report
+go tool pprof http://localhost:8082/debug/pprof/profile
+```
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"sync"
 )
@@ -21,6 +22,10 @@ type Config struct {
 	WS struct {
 		Enabled bool   `long:"ws-enable" description:"enable websocket"`
 		Bind    string `long:"ws-bind" description:"address and port to bind to" default:":8081"`
+	}
+	Pprof struct {
+		Enable bool   `long:"pprof-enable" description:"enable pprof"`
+		Bind   string `long:"pprof-bind" description:"address and port to bind to" default:":8082"`
 	}
 }
 
@@ -63,7 +68,37 @@ func main() {
 
 	startLogServer(&wg, multicastGroup.Join())
 
+	if config.Pprof.Enable {
+		startPprofServer(&wg)
+	}
+
 	wg.Wait()
+}
+
+func startPprofServer(wg *sync.WaitGroup) {
+	httpServer := &http.Server{
+		Addr: config.Pprof.Bind,
+	}
+
+	r := http.NewServeMux()
+
+	// Register pprof handlers
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	httpServer.Handler = r
+
+	gracefulHTTP := gracefulhttp.New(httpServer, 5)
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		gracefulHTTP.Run()
+	}(wg)
 }
 
 func startHTTPServer(wg *sync.WaitGroup, multicastGroup *multicast.Group) {
