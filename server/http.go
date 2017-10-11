7d7095265cb0f9cb28fb9a7b4875df5d0f29c6b6
Marcel Beck <marcel@beck.im>
2017-10-11 19:14:53 +0200

    Add more http request informations

--- server/http.go
+++ server/http.go
@@ -5,23 +5,30 @@ import (
 	"github.com/rs/zerolog"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 type Log struct {
-	Time   string
-	Proto  string
-	Uri    string
-	Method string
-	Header http.Header
-	Body   string
+	Time          string
+	Proto         string
+	Host          string
+	Uri           string
+	Method        string
+	Header        http.Header
+	ContentLength int64
+	Body          string
+	Form          url.Values
+	PostForm      url.Values
 }
 
 func (l *Log) MarshalZerologObject(e *zerolog.Event) {
 	e.
 		Str("Proto", l.Proto).
+		Str("Host", l.Host).
 		Str("Uri", l.Uri).
 		Str("Method", l.Method).
+		Int64("ContentLength", l.ContentLength).
 		Str("Body", l.Body).
 		Msg("Request")
 }
@@ -46,12 +53,16 @@ func (s *HTTPServer) catchAll(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 
 	logMessage := Log{
-		Time:   time.Now().Format(time.RFC3339Nano),
-		Proto:  r.Proto,
-		Uri:    r.RequestURI,
-		Method: r.Method,
-		Header: r.Header,
-		Body:   string(body),
+		Time:          time.Now().Format(time.RFC3339Nano),
+		Proto:         r.Proto,
+		Host:          r.Host,
+		Uri:           r.RequestURI,
+		Method:        r.Method,
+		Header:        r.Header,
+		ContentLength: r.ContentLength,
+		Body:          string(body),
+		Form:          r.Form,
+		PostForm:      r.PostForm,
 	}
 
 	s.multicastGroup.Send(logMessage)
